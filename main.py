fa1bba14da3cde44fb32dbdbb260096c6f287383
Artem Rys <rysartem@gmail.com>
2020-03-06 18:41:50 +0100

    2to3 automatic update.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def greet(name):
-    print "Hello, {0}!".format(name)
-print "What's your name?"
-name = raw_input()
+    print("Hello, {0}!".format(name))
+print("What's your name?")
+name = input()
 greet(name)
